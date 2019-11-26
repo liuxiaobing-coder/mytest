852649992d043534310a676a3e9f8abbc7666787
liuxiaobing-coder <58198974+liuxiaobing-coder@users.noreply.github.com>
2019-11-26 15:31:38 +0800

    Update Hello.java

--- idea-git-test/src/com/kgc/Hello.java
+++ idea-git-test/src/com/kgc/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("commit");
         System.out.println("commit");
         System.out.println("commit");
+        System.out.println("others update");
     }
 }

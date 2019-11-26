f5f93e361faacd7cc59865e69b1d8f8cf31a809f
lxb <1281497577@qq.com>
2019-11-26 15:25:58 +0800

    添加test工程至git

---
+++ idea-git-test/src/com/kgc/Hello.java
@@ -0,0 +1,9 @@
+package com.kgc;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("commit");
+        System.out.println("commit");
+        System.out.println("commit");
+    }
+}
